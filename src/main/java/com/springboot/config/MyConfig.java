308d4675752b67b2d59e43aabd6ed1b8c89809a4
noobHaa <lilei642318580@163.com>
2019-01-23 17:33:09 +0800

    spring boot的国际化处理

--- src/main/java/com/springboot/config/MyConfig.java
+++ src/main/java/com/springboot/config/MyConfig.java
@@ -1,8 +1,10 @@
 package com.springboot.config;
 
 
+import com.springboot.resolver.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -36,4 +38,8 @@ public class MyConfig extends WebMvcConfigurerAdapter {
         return adapter;
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new MyLocaleResolver();
+    }
 }
---
+++ src/main/java/com/springboot/resolver/MyLocaleResolver.java
@@ -0,0 +1,35 @@
+package com.springboot.resolver;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+
+/**
+ *
+ * @ProjectName: springboot-web1
+ * @Package: com.springboot.resolver
+ * @ClassName: MyLocaleResolver
+ * @Author: gnnt
+ * @Date: 2019/1/23 17:00
+ * @Version: 1.0
+ */
+public class MyLocaleResolver implements LocaleResolver {
+    @Override
+    public Locale resolveLocale(HttpServletRequest httpServletRequest) {
+        String l = httpServletRequest.getParameter("l");
+        Locale locale = Locale.getDefault();
+        if (!StringUtils.isEmpty(l)) {
+            String[] split = l.split("_");
+            locale = new Locale(split[0], split[1]);
+        }
+        return locale;
+    }
+
+    @Override
+    public void setLocale(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Locale locale) {
+
+    }
+}
